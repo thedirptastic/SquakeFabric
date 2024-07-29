236208c59e00fccdaa26d107693b328eab15df29
thedirptastic <80712226+thedirptastic@users.noreply.github.com>
2024-07-29 13:03:25 -0700

    Add files via upload

---
+++ src/main/java/com/derp/squake/ISquakeEntity.java
@@ -0,0 +1,13 @@
+package com.derp.squake;
+
+public interface ISquakeEntity
+{
+	int getDisabledMovementTicks_Squake();
+	
+	void setDisabledMovementTicks_Squake(int amt);
+	
+	default boolean shouldReturnMovement_Squake()
+	{
+		return getDisabledMovementTicks_Squake() > 0;
+	}
+}
